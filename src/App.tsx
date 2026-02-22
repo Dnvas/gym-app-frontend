8596e13c290032b5e3d58c55108feb307f3aa180
Dnvas <dnvasilev123@gmail.com>
2026-02-22 03:37:15 +0000

    SEDP-40 Wrap app with WorkoutProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+// src/App.tsx
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { AuthProvider } from './contexts/AuthContext'
+import { WorkoutProvider } from './contexts/WorkoutContext'
 import { RootNavigator } from './navigation'
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <AuthProvider>
-        <StatusBar style="dark" />
-        <RootNavigator />
+        <WorkoutProvider>
+          <StatusBar style="dark" />
+          <RootNavigator />
+        </WorkoutProvider>
       </AuthProvider>
     </SafeAreaProvider>
   )
